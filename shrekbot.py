d415763f9f2d99bb928c246f916566ab24b20373
Afreaux <50078408+Afreaux@users.noreply.github.com>
2021-07-08 10:22:50 -0300

    Update shrekbot.py

--- shrekbot.py
+++ shrekbot.py
@@ -1,11 +1,10 @@
 import pyautogui, time
-time.sleep(10)
+time.sleep(6)
 f = open("shrek", "r")
 
-...
 for word in f:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
     time.sleep(6)
-...
+
 
